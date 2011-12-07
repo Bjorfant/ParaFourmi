062d9c9c198d77dd0f31266fc2bc34e86bea6153
Thomas Belin <ThomasBelin4@gmail.com>
2011-12-07 20:38:28 +0100

    amélioration des fonctions de récupération de voisins (factorisation de code) +

--- testpara.py
+++ testpara.py
@@ -1,17 +1,19 @@
-#-*- coding:latin1 -*-
+#-*- coding:UTF-8 -*-
 
 from datapara import *
 import random
+import os
 
-'''
-0 : vide
-1 : accessible
-2 : plein
-3 : grain
-4 : fourmi
-5 : fourmi-grain
-'''
+#constante determinant l'état de chaque bloc
+class State:
+	VIDE=0
+	ACCESSIBLE=1
+	PLEIN=2
+	GRAIN=3
+	FOURMI=4
+	TRANSIT=5
 
+#affichage de la matrice
 def printMatrix(mat):
     buf = ""
     for i in range(0, len(mat)):
@@ -22,15 +24,7 @@ def printMatrix(mat):
    	 buf += str(mat[i])
     print(buf)
 
-taille = 3
-matfourmi = [0,0,0,1,1,1,3,3,3,0,4,0,1,1,1,3,3,3,0,0,0,1,1,1,3,3,3]
-matfourmi2 = [0]*(taille**3)
-matfourmi3 = [0]*(taille**3)
-print("Matrice initiale")
-printMatrix(matfourmi)
 
-
-	
 #retourne vrai si la case d'indice "index" est sur le bord gauche de la matrice
 def isOnLeftBorder(index):
 	return index%taille == 0
@@ -39,11 +33,11 @@ def isOnLeftBorder(index):
 def isOnRightBorder(index):
 	return index%(taille - 1) == 0
 
-#retourne vrai si la case d'indice "index" est sur le bord sup�rieur de la matrice
+#retourne vrai si la case d'indice "index" est sur le bord supérieur de la matrice
 def isOnTopBorder(index):
 	return index%(taille**2) - taille < 0
 
-#retourne vrai si la case d'indice "index" est sur le bord inf�rieur de la matrice
+#retourne vrai si la case d'indice "index" est sur le bord inférieur de la matrice
 def isOnBottomBorder(index):
 	return index%(taille**2) + taille >= taille**2
 
@@ -51,61 +45,94 @@ def isOnBottomBorder(index):
 def isOnFrontBorder(index):
 	return index + (taille**2) >= taille**3
 
-#retourne vrai si la case d'indice "index" est sur le bord arri�re de la matrice
+#retourne vrai si la case d'indice "index" est sur le bord arrière de la matrice
 def isOnBackBorder(index):
 	return index < taille**2
+
+
+def isAccessible(case):
+	# Calcul du nombre de voisins
+	voisins=[]
+	if not isOnRightBorder(case):
+		if matfourmi3[case+1] == State.PLEIN or matfourmi3[case+1] == State.GRAIN:
+			return True
+	if not isOnLeftBorder(case):
+		if matfourmi3[case-1] == State.PLEIN or matfourmi3[case-1] == State.GRAIN:
+			return True
+	if not isOnTopBorder(case):
+		if matfourmi3[case-taille] == State.PLEIN or matfourmi3[case-taille] == State.GRAIN:
+			return True
+	if not isOnBottomBorder(case):
+		if matfourmi3[case+taille] == State.PLEIN or matfourmi3[case+taille] == State.GRAIN:
+			return True
+	if not isOnFrontBorder(case):
+		if matfourmi3[case+taille**2] == State.PLEIN or matfourmi3[case+taille**2] == State.GRAIN:
+			return True
+	if not isOnBackBorder(case):
+		if matfourmi3[case-taille**2] == State.PLEIN or matfourmi3[case-taille**2] == State.GRAIN:
+			return True
+	return False
+
+def isGrain(case):
+	# Calcul du nombre de voisins
+	voisins=[]
+	if not isOnRightBorder(case):
+		if matfourmi3[case+1] == State.VIDE or matfourmi3[case+1] == State.ACCESSIBLE:
+			return True
+	if not isOnLeftBorder(case):
+		if matfourmi3[case-1] == State.VIDE or matfourmi3[case-1] == State.ACCESSIBLE:
+			return True
+	if not isOnTopBorder(case):
+		if matfourmi3[case-taille] == State.VIDE or matfourmi3[case-taille] == State.ACCESSIBLE:
+			return True
+	if not isOnBottomBorder(case):
+		if matfourmi3[case+taille] == State.VIDE or matfourmi3[case+taille] == State.ACCESSIBLE:
+			return True
+	if not isOnFrontBorder(case):
+		if matfourmi3[case+taille**2] == State.VIDE or matfourmi3[case+taille**2] == State.ACCESSIBLE:
+			return True
+	if not isOnBackBorder(case):
+		if matfourmi3[case-taille**2] == State.VIDE or matfourmi3[case-taille**2] == State.ACCESSIBLE:
+			return True
+	return False
 
 def deplacement_alea(voisins):
 	if len(voisins) >= 1:
 		return random.choice(voisins)
 	else:
 		return -1
-		
-def listevoisinsaccessibles(case):
-	# Calcul du nombre de voisins
+
+#récupère les voisins d'une case de la matrice
+#possibilité de filtrer les voisins par une liste d'état que l'on cherche 
+#si la liste est vide on renvoit tous les voisins
+def listeVoisins(index, filtre):
 	voisins=[]
-	if not isOnRightBorder(case):
-		if matfourmi[case+1] == 1:
-			voisins.append(case+1)
-	if not isOnLeftBorder(case):
-		if matfourmi[case-1] == 1:
-			voisins.append(case-1)
-	if not isOnTopBorder(case):
-		if matfourmi[case-taille] == 1:
-			voisins.append(case-taille)
-	if not isOnBottomBorder(case):
-		if matfourmi[case+taille] == 1:
-			voisins.append(case+taille)
-	if not isOnFrontBorder(case): # A reformuler
-		if matfourmi[case+taille**2] == 1:
-			voisins.append(case+taille**2)
-	if not isOnBackBorder(case): # A reformuler
-		if matfourmi[case-taille**2] == 1:
-			voisins.append(case-taille**2)
+	all = filtre == [] #verifie si il a une condition
+	if not isOnRightBorder(index):
+		if all or matfourmi[index+1] in filtre:
+			voisins.append(index+1)
+	if not isOnLeftBorder(index):
+		if all or matfourmi[index-1] in filtre:
+			voisins.append(index-1)
+	if not isOnTopBorder(index):
+		if all or matfourmi[index-taille] in filtre:
+			voisins.append(index-taille)
+	if not isOnBottomBorder(index):
+		if all or matfourmi[index+taille] in filtre:
+			voisins.append(index+taille)
+	if not isOnFrontBorder(index):
+		if all or matfourmi[index+taille**2] in filtre:
+			voisins.append(index+taille**2)
+	if not isOnBackBorder(index):
+		if all or matfourmi[index-taille**2] in filtre:
+			voisins.append(index-taille**2)
 	return voisins
 
-def listefourmisvoisines(case):
-	# Calcul du nombre de voisins
-	voisins=[]
-	if not isOnRightBorder(case):
-		if matfourmi[case+1] == 4:
-			voisins.append(case+1)
-	if not isOnLeftBorder(case):
-		if matfourmi[case-1] == 4:
-			voisins.append(case-1)
-	if not isOnTopBorder(case):
-		if matfourmi[case-taille] == 4:
-			voisins.append(case-taille)
-	if not isOnBottomBorder(case):
-		if matfourmi[case+taille] == 4:
-			voisins.append(case+taille)
-	if not isOnFrontBorder(case): # A reformuler
-		if matfourmi[case+taille**2] == 4:
-			voisins.append(case+taille**2)
-	if not isOnBackBorder(case): # A reformuler
-		if matfourmi[case-taille**2] == 4:
-			voisins.append(case-taille**2)
-	return voisins
+def listeVoisinsAccessibles(index):
+	return listeVoisins(index, [State.ACCESSIBLE])
+
+def listeVoisinsActifs(case):
+	return listeVoisins(index, [State.FOURMI, State.TRANSIT])
 	
 def etatfourmivoisine(case):
 	# Calcul du nombre de voisins
@@ -131,10 +158,9 @@ def etatfourmivoisine(case):
 
 def transition(case):
 	etat=matfourmi[case]
-	
-	voisins = listevoisinsaccessibles(case)
-	nbvoisins = len(voisins)
 	if etat==4 or etat==5:
+		voisins = listeVoisinsAccessibles(case)
+		print("Liste des voisins accessibles :",voisins)
 		return deplacement_alea(voisins)
 	else:
 		return -1
@@ -147,10 +173,56 @@ def transition2(case):
 	else:
 		return matfourmi[case]
 
-matfourmi2 = map1 ( transition, range(taille**3) )[0]
-print("\nMatrice temporaire")
-printMatrix(matfourmi2)
+def updateStates(case):
+	state = matfourmi3[case]
+	if state == State.VIDE or state == State.ACCESSIBLE:
+		if isAccessible(case):
+			return State.ACCESSIBLE
+		else:
+			return State.VIDE
+	if state == State.PLEIN or state == State.GRAIN:
+		if isGrain(case):
+			return State.GRAIN
+		else:
+			return State.PLEIN
+	else:	
+		return state
 
-matfourmi3 = map1(transition2, range(taille**3))[0]
-print("\nMatrice T+1")
-printMatrix(matfourmi3)
+
+##########################################
+################ MAIN ####################
+##########################################
+clear = lambda: os.system('clear')
+
+taille = 3
+matfourmi =[0,0,0,
+1,1,1,
+3,3,3,
+0,0,0,
+1,4,1,
+3,3,3,
+0,0,0,
+1,1,1,
+3,3,3]
+matfourmi2 = [0]*(taille**3)
+matfourmi3 = [0]*(taille**3)
+
+nbEtapes = input("Combien d'étapes voulez vous réaliser ? \n")
+nbEtapes = int(nbEtapes)
+print("Matrice initiale")
+printMatrix(matfourmi)
+
+for i in range(0, nbEtapes):
+	clear()
+	print("\nMatrice temps", str(i))
+	matfourmi2 = map1 (transition, range(taille**3) )[0]
+	print("\nMatrice temporaire")
+	printMatrix(matfourmi2)
+	
+	matfourmi = map1(transition2, range(taille**3))[0]
+	#print("\nMatrice T+1")
+	#printMatrix(matfourmi3)
+	
+	#matfourmi = map1(updateStates, range(taille**3))[0]
+	printMatrix(matfourmi)
+	input("press your penis to continue")
